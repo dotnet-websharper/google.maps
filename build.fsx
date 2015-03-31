ebe2a33080f2efca21ab0c4e68d200973319a675
András Jankó <andras.janko@gmail.com>
2015-03-31 19:42:20 +0200

    remove alpha tag

--- build.fsx
+++ build.fsx
@@ -2,7 +2,7 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("WebSharper.Google.Maps", "3.0-alpha")
+    BuildTool().PackageId("WebSharper.Google.Maps", "3.0")
         .References(fun r -> [r.Assembly "System.Web"])
     |> fun bt -> bt.WithFramework(bt.Framework.Net40)
 
