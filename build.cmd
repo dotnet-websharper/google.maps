b7a155255915ebe58e68dafcbd2a43ffef4f82a0
András Jankó <andras.janko@gmail.com>
2021-01-03 15:39:29 +0100

    Update to dotnet Fake/Paket tooling

--- build.cmd
+++ build.cmd
@@ -1,10 +1,10 @@
 @echo off
 setlocal
-
-.paket\paket.exe restore -g build
+    
+dotnet tool restore
+dotnet paket restore
+dotnet restore
 if errorlevel 1 exit /b %errorlevel%
-
-.paket\paket.exe update -g wsbuild
-if errorlevel 1 exit /b %errorlevel%
-
-call paket-files\wsbuild\github.com\dotnet-websharper\build-script\WebSharper.Fake.cmd %*
+    
+call paket-files\wsbuild\github.com\dotnet-websharper\build-script\update.cmd
+call paket-files\wsbuild\github.com\dotnet-websharper\build-script\build.cmd %*
--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-set -e
-
-if [ "$OS" = "Windows_NT" ]; then
-    .paket/paket.exe restore -g build
-    .paket/paket.exe update -g wsbuild
-else
-    mono .paket/paket.exe restore -g build
-    mono .paket/paket.exe update -g wsbuild
-fi
-
-exec paket-files/wsbuild/github.com/dotnet-websharper/build-script/WebSharper.Fake.sh "$@"
+    
+dotnet tool restore
+dotnet paket restore
+dotnet restore
+    
+. paket-files/wsbuild/github.com/dotnet-websharper/build-script/update.sh
+. paket-files/wsbuild/github.com/dotnet-websharper/build-script/build.sh "$@"
