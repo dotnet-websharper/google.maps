5cf147a94edea853d69e3c295ac63db0fecc7d7f
Got <souvanxay38@gmail.com>
2025-06-23 11:34:08 +0200

    change project into esbuild bundling

---
+++ WebSharper.Google.Maps.Tests/esbuild.config.mjs
@@ -0,0 +1,25 @@
+import { existsSync, cpSync, readdirSync } from 'fs'
+import { build } from 'esbuild'
+
+if (existsSync('./build/Content/WebSharper/')) {
+    cpSync('./build/Content/WebSharper/', './wwwroot/Content/WebSharper/', { recursive: true });
+}
+
+const files = readdirSync('./build/Scripts/WebSharper/WebSharper.Google.Maps.Tests/');
+
+files.forEach(file => {
+    if (file.endsWith('.js')) {
+        var options =
+        {
+            entryPoints: ['./build/Scripts/WebSharper/WebSharper.Google.Maps.Tests/' + file],
+            bundle: true,
+            minify: true,
+            format: 'iife',
+            outfile: 'wwwroot/Scripts/WebSharper/' + file,
+            globalName: 'wsbundle'
+        };
+
+        console.log("Bundling:", file);
+        build(options);
+    }
+});
